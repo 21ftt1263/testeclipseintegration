08c9b578ac5e31db9cb775bb868f89a84807e2e8
Nabihah Abdullah <21FTT1263@student.pb.edu.bn>
2023-01-31 15:27:55 +0800

    Trying basic OOP

--- testeclispeintegration/src/testeclispeintegration/test/Test.java
+++ testeclispeintegration/src/testeclispeintegration/test/Test.java
@@ -3,5 +3,7 @@ package testeclispeintegration.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("GitHub");
+		ClassA classA = new ClassA("ABCDEF");
+		System.out.println(classA.getAbc());
 	}
 }
